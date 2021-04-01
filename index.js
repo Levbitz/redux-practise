5e3bfbcbbc8777734c3cdc9932e1d7669e771ca3
Levbitz <maxinosamx@gmail.com>
2021-04-01 06:54:25 +0530

    logged

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const redux = require("redux");
+
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
-
 //action creater
-
 const buyCake = () => {
   return (
     //action
@@ -11,3 +13,34 @@ const buyCake = () => {
     }
   );
 };
+
+//reducer
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("Initial State", store.getState());
+const unsubsribe = store.subscribe(() =>
+  console.log("Upadted state", store.getState())
+);
+
+store.dispatch(buyCake());
+
+store.dispatch(buyCake());
+
+store.dispatch(buyCake());
+
+unsubsribe();
